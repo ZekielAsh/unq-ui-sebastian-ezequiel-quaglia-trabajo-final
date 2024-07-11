f41df4fa464d03cb8096eeca4c2462b9adf0c516
Ezequiel Quaglia <sherlock-ezequiel@hotmail.com>
2024-07-10 21:24:50 -0300

    Question y actualizacion en app
    Error en question que cuando clickeo una respuesta no lo registra

---
+++ src/pages/answer/Answer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Answer.css';
+
+const Answer = ({ answers, whenAnswer }) => {
+
+    const handleClick = (option) => {
+        console.log("Option clicked:", option);
+        whenAnswer(option);
+    };
+
+    return (
+        <>
+            <div className='answer-container'>
+                <button onClick={() => handleClick("option1")}> {answers.option1} </button>
+                <button onClick={() => handleClick("option2")}> {answers.option2} </button>
+                <button onClick={() => handleClick("option3")}> {answers.option3} </button>
+                <button onClick={() => handleClick("option4")}> {answers.option4} </button>
+            </div>
+        </>
+    );
+}
+
+export default Answer;
---
+++ src/pages/question/Question.jsx
@@ -0,0 +1,67 @@
+import "./Question.css";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Answer from "../answer/Answer";
+import Api from "../../service/Api";
+
+const Question = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { difficulty } = location.state; 
+    const [questionList, setQuestionList] = useState([]);
+    const [counter, setCounter] = useState(0);
+    const [correct, setCorrect] = useState(null);
+    const [answers, setAnswers] = useState(0);
+
+    useEffect(() => {
+        const fetchQuestion = async () => {
+            try {
+                const response = await Api.getQuestions(difficulty);
+                setQuestionList(response.data);
+            } catch (error) {
+                console.error('Error fetching question', error);
+            }
+        };
+        fetchQuestion();
+    }, []);
+
+    const handleAnswer = async (answer) => {
+        console.log("Answer received:", answer);
+        const questId = {
+            questionId: questionList[counter].id, 
+            ret: answer
+        };
+        try {
+            const response = await Api.postAnswer(questId);
+            setCorrect(response.data.answer ? "Respuesta Correcta!" : "Respuesta Incorrecta!");
+            setAnswers(current => current + (response.data.answer ? 1 : 0));
+            setTimeout(() => {
+                setCorrect(null);
+                setCounter(previous => previous + 1);
+              }, 1000);
+        } catch (error) {
+            console.error('Error posting answer', error);
+        }
+    };
+
+    if (!questionList.length) { return <div>Cargando...</div>; };
+
+    if (counter >= questionList.length) { 
+        navigate("/results", { state: { answers } });
+    }
+
+    return (
+        <div className="question-container">
+            <div className="results">
+                Puntaje: {answers}
+            </div>
+            {correct && <div className="correct">{correct}</div>};
+            <div>
+                <div className="question">{questionList[counter].question}</div>
+                <Answer answers={questionList[counter]} whenAnswer={handleAnswer} />
+            </div>
+        </div>
+    );
+}
+
+export default Question;
